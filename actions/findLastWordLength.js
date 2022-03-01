d90646948713a8ecfc0d0c111f47a0e5eba60eb8
arielwb <arielwb@gmail.com>
2022-02-28 21:36:06 -0300

    fix: add check for constraint

--- actions/findLastWordLength.js
+++ actions/findLastWordLength.js
@@ -1,4 +1,8 @@
 export const findLastWordLength = (string) => {
+  if (string.length < 1 || string.length > 104) {
+    return null
+  }
+
   const getLastWord = /(\w+)\s*$/
   const lastWord = getLastWord.exec(string)[1]
 
--- actions/findLastWordLength.test.js
+++ actions/findLastWordLength.test.js
@@ -10,4 +10,14 @@ describe('findLastWordLength', () => {
     const lastWordLength = findLastWordLength(' fly me    to   the moon   ')
     expect(lastWordLength).toEqual(4)
   })
+
+  it('should return null if the string has a length smaller then 1', () => {
+    const lastWordLength = findLastWordLength('')
+    expect(lastWordLength).toEqual(null)
+  })
+
+  it('should return null if the string has a length smaller then 104', () => {
+    const lastWordLength = findLastWordLength('Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore')
+    expect(lastWordLength).toEqual(null)
+  })
 })
